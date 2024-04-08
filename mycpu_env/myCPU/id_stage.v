2a5f9a72b8519b25523da1bfb0cc701c18880517
Janet Baker <guoxiangyuan1@gmail.com>
2024-04-08 15:15:50 +0800

    fix4
    跳转时需要及时丢弃运算结果

--- mycpu_env/myCPU/id_stage.v
+++ mycpu_env/myCPU/id_stage.v
@@ -132,6 +132,9 @@ always @(posedge clk) begin
     else if (es_allowin) begin
         ds_valid <= fs_to_ds_valid;
     end
+    if (br_taken) begin
+        ds_valid <= 1'b0;
+    end
 
     if (fs_to_ds_valid && ds_allowin) begin
         fs_to_ds_bus_r <= fs_to_ds_bus;
