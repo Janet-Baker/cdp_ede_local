6a6f018b57984978230a5f908062ad43584c5867
Janet Baker <guoxiangyuan1@gmail.com>
2024-04-08 09:34:23 +0800

    remark
    加了注释，没有实质更改

--- mycpu_env/myCPU/mycpu_top.v
+++ mycpu_env/myCPU/mycpu_top.v
@@ -107,6 +107,7 @@ wire [31:0] ms_final_result;
 assign seq_pc       = fs_pc + 3'h4;
 assign nextpc       = br_taken ? br_target : seq_pc;
 
+// 取指阶段
 always @(posedge clk) begin
     if (reset) begin
         pc <= 32'h1c000000; 
@@ -121,11 +122,14 @@ assign inst_sram_addr  = pc;
 assign inst_sram_wdata = 32'b0;
 assign inst            = inst_sram_rdata;
 
+// 译码阶段，拆分操作码和操作数
+// 拆分可能的操作码。如果指令比较长，可以将它们组合起来使用。
 assign op_31_26  = inst[31:26];
 assign op_25_22  = inst[25:22];
 assign op_21_20  = inst[21:20];
 assign op_19_15  = inst[19:15];
 
+// 分类，拆分可能的操作数
 assign rd   = inst[ 4: 0];
 assign rj   = inst[ 9: 5];
 assign rk   = inst[14:10];
@@ -135,11 +139,13 @@ assign i20  = inst[24: 5];
 assign i16  = inst[25:10];
 assign i26  = {inst[ 9: 0], inst[25:10]};
 
+// 使用译码器(参考3-8译码器)展开，将指令展开为大数组，方便匹配指令。
 decoder_6_64 u_dec0(.in(op_31_26 ), .out(op_31_26_d ));
 decoder_4_16 u_dec1(.in(op_25_22 ), .out(op_25_22_d ));
 decoder_2_4  u_dec2(.in(op_21_20 ), .out(op_21_20_d ));
 decoder_5_32 u_dec3(.in(op_19_15 ), .out(op_19_15_d ));
 
+// 匹配指令
 assign inst_add_w  = op_31_26_d[6'h00] & op_25_22_d[4'h0] & op_21_20_d[2'h1] & op_19_15_d[5'h00];
 assign inst_sub_w  = op_31_26_d[6'h00] & op_25_22_d[4'h0] & op_21_20_d[2'h1] & op_19_15_d[5'h02];
 assign inst_slt    = op_31_26_d[6'h00] & op_25_22_d[4'h0] & op_21_20_d[2'h1] & op_19_15_d[5'h04];
@@ -161,6 +167,8 @@ assign inst_beq    = op_31_26_d[6'h16];
 assign inst_bne    = op_31_26_d[6'h17];
 assign inst_lu12i_w= op_31_26_d[6'h05] & ~inst[25];
 
+// alu 操作码分类
+// 0: add, addi, ld, st, jirl, bl 都需要走加法器，所以放一起
 assign alu_op[ 0] = inst_add_w | inst_addi_w | inst_ld_w | inst_st_w
                     | inst_jirl | inst_bl;
 assign alu_op[ 1] = inst_sub_w;
@@ -182,19 +190,24 @@ assign need_si20  =  inst_lu12i_w;
 assign need_si26  =  inst_b | inst_bl;
 assign src2_is_4  =  inst_jirl | inst_bl;
 
+// 立即数可能不是很规范
 assign imm = src2_is_4 ? 32'h4                      :
              need_si20 ? {i20[19:0], 12'b0}         :
 /*need_ui5 || need_si12*/{{20{i12[11]}}, i12[11:0]} ;
 
+// 根据操作码，选择不同的偏移量，寻找操作数
 assign br_offs = need_si26 ? {{ 4{i26[25]}}, i26[25:0], 2'b0} :
                              {{14{i16[15]}}, i16[15:0], 2'b0} ;
 
 assign jirl_offs = {{14{i16[15]}}, i16[15:0], 2'b0};
 
+// 如果源操作数是rd?
 assign src_reg_is_rd = inst_beq | inst_bne | inst_st_w;
 
+// 如果源操作数是pc
 assign src1_is_pc    = inst_jirl | inst_bl;
 
+// 如果源操作数是立即数
 assign src2_is_imm   = inst_slli_w |
                        inst_srli_w |
                        inst_srai_w |
@@ -211,6 +224,7 @@ assign gr_we         = ~inst_st_w & ~inst_beq & ~inst_bne & ~inst_b & ~inst_bl;
 assign mem_we        = inst_st_w;
 assign dest          = dst_is_r1 ? 5'd1 : rd;
 
+// 取寄存器的值
 assign rf_raddr1 = rj;
 assign rf_raddr2 = src_reg_is_rd ? rd :rk;
 regfile u_regfile(
@@ -227,6 +241,7 @@ regfile u_regfile(
 assign rj_value  = rf_rdata1;
 assign rkd_value = rf_rdata2;
 
+// 分支判断
 assign rj_eq_rd = (rj_value == rkd_value);
 assign br_taken = (   inst_beq  &&  rj_eq_rd
                    || inst_bne  && !rj_eq_rd
@@ -237,9 +252,11 @@ assign br_taken = (   inst_beq  &&  rj_eq_rd
 assign br_target = (inst_beq || inst_bne || inst_bl || inst_b) ? (ds_pc + br_offs) :
                                                    /*inst_jirl*/ (rj_value + jirl_offs);
 
+// 跳转指令
 assign alu_src1 = src1_is_pc  ? pc[31:0] : rj_value;
 assign alu_src2 = src2_is_imm ? imm : rkd_value;
 
+// alu算数
 alu u_alu(
     .alu_op     (alu_op    ),
     .alu_src1   (alu_src2  ),
@@ -247,11 +264,13 @@ alu u_alu(
     .alu_result (alu_result)
     );
 
+// 访存
 assign data_sram_en    = (rfrom_mem || mem_we) && valid;
 assign data_sram_we    = mem_we;
 assign data_sram_addr  = alu_result;
 assign data_sram_wdata = rkd_value;
 
+// 最后决定是从内存中取值还是从alu中取值
 assign mem_result   = data_sram_rdata;
 assign final_result = res_from_mem ? mem_result : alu_result;
 
