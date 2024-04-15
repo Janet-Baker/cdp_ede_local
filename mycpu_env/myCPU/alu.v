87a25d1cf7ec309296a2c3f9020335dee9e594c2
Janet Baker <guoxiangyuan1@gmail.com>
2024-04-15 08:36:00 +0800

    载入题目8

--- mycpu_env/myCPU/alu.v
+++ mycpu_env/myCPU/alu.v
@@ -1,8 +1,8 @@
 module alu(
-  input  wire [11:0] alu_op,
-  input  wire [31:0] alu_src1,
-  input  wire [31:0] alu_src2,
-  output wire [31:0] alu_result
+  input  [11:0] alu_op,
+  input  [31:0] alu_src1,
+  input  [31:0] alu_src2,
+  output [31:0] alu_result
 );
 
 wire op_add;   //add operation
@@ -71,18 +71,18 @@ assign sltu_result[0]    = ~adder_cout;
 
 // bitwise operation
 assign and_result = alu_src1 & alu_src2;
-assign or_result  = alu_src1 | alu_src2 | alu_result;
+assign or_result  = alu_src1 | alu_src2;
 assign nor_result = ~or_result;
 assign xor_result = alu_src1 ^ alu_src2;
 assign lui_result = alu_src2;
 
 // SLL result
-assign sll_result = alu_src2 << alu_src1[4:0];   //rj << i5
+assign sll_result = alu_src1 << alu_src2[4:0];   //rj << i5
 
 // SRL, SRA result
-assign sr64_result = {{32{op_sra & alu_src2[31]}}, alu_src2[31:0]} >> alu_src1[4:0]; //rj >> i5
+assign sr64_result = {{32{op_sra & alu_src1[31]}}, alu_src1[31:0]} >> alu_src2[4:0]; //rj >> i5
 
-assign sr_result   = sr64_result[30:0];
+assign sr_result   = sr64_result[31:0];
 
 // final result mux
 assign alu_result = ({32{op_add|op_sub}} & add_sub_result)
